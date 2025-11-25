2877a96c039808a6bd4b617feee6601974502e37
GVerse-Dev <44476140+GVerse-Dev@users.noreply.github.com>
2025-11-26 06:13:46 +0900

    [Bronze V] Title: 새싹, Time: 44 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,16 @@
+using System;
+
+class BOJ
+{
+    static void Main()
+    {
+      
+        Console.WriteLine("         ,r'\"7");
+        Console.WriteLine("r`-_   ,'  ,/");
+        Console.WriteLine(" \\. \". L_r'");
+        Console.WriteLine("   `~\\/");
+        Console.WriteLine("      |");
+        Console.WriteLine("      |");
+
+    }
+}
