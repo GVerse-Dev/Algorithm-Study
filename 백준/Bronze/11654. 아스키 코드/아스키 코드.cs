955e8d03bbac96abc07657e2bb937c829241fa19
GVerse-Dev <44476140+GVerse-Dev@users.noreply.github.com>
2025-11-26 06:12:02 +0900

    [Bronze V] Title: 아스키 코드, Time: 48 ms, Memory: 5444 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,14 @@
+using System;
+
+class BOJ
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        int num1 = input[0];
+      
+        Console.WriteLine(num1);
+
+    }
+}
