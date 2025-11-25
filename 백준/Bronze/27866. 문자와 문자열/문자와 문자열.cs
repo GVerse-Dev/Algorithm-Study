e5fc3aa74ab76d4fafaa3bcda52e205f0f5dfc5a
GVerse-Dev <44476140+GVerse-Dev@users.noreply.github.com>
2025-11-26 06:14:12 +0900

    [Bronze V] Title: 문자와 문자열, Time: 44 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cs
@@ -0,0 +1,13 @@
+using System;
+
+class BOJ
+{
+    static void Main()
+    {
+
+        string str = Console.ReadLine();
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(str[n-1]);
+    }
+}
