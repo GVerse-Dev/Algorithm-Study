f1dcbd7764c03cd1007f46776d86799c9c1dadf2
GVerse-Dev <44476140+GVerse-Dev@users.noreply.github.com>
2025-11-26 06:07:14 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 188 ms, Memory: 6564 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+class BOJ
+{
+
+    static void Main()
+    {
+        int count = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= count; i++) 
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
