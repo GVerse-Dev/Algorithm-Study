33c87aebebfb00f89c3a4ba9bf1e8f46328356ec
GVerse-Dev <44476140+GVerse-Dev@users.noreply.github.com>
2025-12-12 03:23:04 +0900

    [Silver IV] Title: 수 찾기, Time: 156 ms, Memory: 30012 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cs
+++ 백준/Silver/1920. 수 찾기/수 찾기.cs
@@ -7,45 +7,6 @@ using System.ComponentModel.Design;
 class BOJ
 {
 
-    static void MergeSort(int[] arrOrigin, int[] tempArr, int l, int r)
-    {
-        if (l < r)
-        {
-            int m = (l + r) / 2;
-            MergeSort(arrOrigin, tempArr, l, m);
-            MergeSort(arrOrigin, tempArr, m + 1, r);
-            Merge(arrOrigin, tempArr, l, r, m + 1);
-        }
-    }
-
-    static void Merge(int[] arr, int[] tempArr, int start, int end, int mid)
-    {
-        if (start < end)
-        {
-            int tmp1 = start;       //왼쪽
-            int tmp2 = mid;         //오른쪽
-            int index = start;      //합친 배열의 인덱스
-            while (tmp1 < mid && tmp2 <= end)
-            {
-                if (arr[tmp1] <= arr[tmp2])
-                    tempArr[index++] = arr[tmp1++];
-                else
-                    tempArr[index++] = arr[tmp2++];
-            }
-
-            while (tmp1 < mid)
-                tempArr[index++] = arr[tmp1++];
-            while (tmp2 <= end)
-                tempArr[index++] = arr[tmp2++];
-
-            for (int i = start; i <= end; i++)
-            {
-                arr[i] = tempArr[i];
-            }
-        }
-
-    }
-
     static int BinarySearch(int[] arr, int l, int r, int target)
     {
         int mid = (l + r) / 2;
@@ -84,9 +45,7 @@ class BOJ
             inputArr[i] = int.Parse(str[i]);
         }
 
-        int[] tempArr = new int[inputCnt];
-        MergeSort(inputArr, tempArr, 0, inputCnt - 1);
-
+        Array.Sort(inputArr);
 
         int inputTargetCnt = int.Parse(Console.ReadLine());
         int[] inputTarget = new int[inputTargetCnt];
