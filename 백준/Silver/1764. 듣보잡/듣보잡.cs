14eb56fbc8e6e33c3e1a52328e8a17927eadc642
GVerse-Dev <44476140+GVerse-Dev@users.noreply.github.com>
2025-12-16 21:52:49 +0900

    [Silver IV] Title: 듣보잡, Time: 92 ms, Memory: 20816 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cs
+++ 백준/Silver/1764. 듣보잡/듣보잡.cs
@@ -13,30 +13,22 @@ class BOJ
         int inputMCnt = int.Parse(input[1]);
 
         Dictionary<string, int> map = new Dictionary<string, int>();
+        List<string> list = new List<string>();
 
         for (int i = 0; i < inputNCnt + inputMCnt; ++i)
         {
             string inputName = Console.ReadLine();
             if (map.ContainsKey(inputName))
-                map[inputName] = 2;
-            else
-                map[inputName] = 1;
+                list.Add(inputName);
+
+            map[inputName] = i;
         }
 
         StringBuilder sb = new StringBuilder();
-
-        int cnt = 0;
-        List<string> list = new List<string>();
-        foreach(var item in map) 
-        {
-            if (item.Value == 2)
-            {
-                cnt++;
-                list.Add(item.Key);
-            }
-        }
+        
         list.Sort();
-        sb.Insert(0, cnt + "\n");
+
+        sb.Insert(0, list.Count + "\n");
         foreach (var item in list)
         {
             sb.Append(item + "\n");
