2264ea94129d63fdd4aaacc80cea1b87dea867e8
GVerse-Dev <44476140+GVerse-Dev@users.noreply.github.com>
2026-01-13 23:24:50 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 48 ms, Memory: 5564 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cs
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cs
@@ -7,35 +7,31 @@ using System.ComponentModel.Design;
 class BOJ
 {
 
-    static int DFS(int target, int value)
+    static void DP()
     {
-        int result = 0;
-
-        if (value == target)
-            return 1;
-
-        if (value > target)
-            return 0;
-
-        result += DFS(target, value + 1);
-        result += DFS(target, value + 2);
-        result += DFS(target, value + 3);
-
-        return result; 
+        
     }
 
     static void Main()
     {
         int inputCnt = int.Parse(Console.ReadLine());
 
-        StringBuilder sb = new StringBuilder();
+        int[] ints = new int[11];
 
-        
+        ints[0] = 0;
+        ints[1] = 1;
+        ints[2] = 2;
+        ints[3] = 4;
+
+        for (int i = 4; i < ints.Length; i++)
+        {
+            ints[i] = ints[i - 1] + ints[i - 2] + ints[i - 3];
+        }
 
         for(int i = 0; i < inputCnt; i++) 
         {
             int target = int.Parse(Console.ReadLine());
-            Console.WriteLine(DFS(target, 1) + DFS(target, 2) + DFS(target, 3));
+            Console.WriteLine(ints[target]);
         }
 
        
