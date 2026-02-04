6ba064b4b7c1935cb2e6e49882842fe261e55ff8
GVerse-Dev <44476140+GVerse-Dev@users.noreply.github.com>
2026-02-04 21:48:44 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 316 ms, Memory: 26644 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cs
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cs
@@ -3,7 +3,6 @@ using System.Text;
 class BOJ
 {
     static StringBuilder sb = new StringBuilder();
-    
 
     static void Main()
     {
@@ -12,51 +11,50 @@ class BOJ
         int m = int.Parse(inputNM[1]);
         
         int[,] board = new int[n,n];
-        int[,] prefixSum = new int[n, n];
-        for (int col = 0; col < n; col++)
+        for (int row = 0; row < n; row++)
         {
             string[] input = Console.ReadLine().Split(" ");
-            for (int row = 0; row < input.Length; row++)
+            for (int col = 0; col < input.Length; col++)
             {
-                int value = int.Parse(input[row]);
-                board[col, row] = value;
+                int value = int.Parse(input[col]);
+                board[row, col] = value;
                 int leftRightSquare = 0;
-                if (col - 1 >= 0)
-                    leftRightSquare = board[col - 1, row];
+                if (row - 1 >= 0)
+                    leftRightSquare = board[row - 1, col];
 
                 int topBottomSquare = 0;
-                if (row - 1 >= 0)
-                    topBottomSquare = board[col, row - 1];
+                if (col - 1 >= 0)
+                    topBottomSquare = board[row, col - 1];
 
                 int duplicateSquare = 0;
-                if (col - 1 >= 0 && row - 1 >= 0)
-                    duplicateSquare = board[col - 1, row - 1];
+                if (row - 1 >= 0 && col - 1 >= 0)
+                    duplicateSquare = board[row - 1, col - 1];
 
-                board[col, row] += leftRightSquare + topBottomSquare - duplicateSquare;
+                board[row, col] += leftRightSquare + topBottomSquare - duplicateSquare;
             }
         }
 
         for (int i = 0; i < m; i++)
         {
             string[] position = Console.ReadLine().Split(" ");
-            int col1 = int.Parse(position[0]) - 1;
-            int row1 = int.Parse(position[1]) - 1;
-            int col2 = int.Parse(position[2]) - 1;
-            int row2 = int.Parse(position[3]) - 1;
+            int col1 = int.Parse(position[1]) - 1;
+            int row1 = int.Parse(position[0]) - 1;
+            int col2 = int.Parse(position[3]) - 1;
+            int row2 = int.Parse(position[2]) - 1;
 
             int leftRightSquare = 0;
-            if (col1 - 1 >= 0)
-                leftRightSquare = board[col1 - 1, row2];
+            if (row1 - 1 >= 0)
+                leftRightSquare = board[row1 - 1, col2];
 
             int topBottomSquare = 0;
-            if (row1 - 1 >= 0)
-                topBottomSquare = board[col2, row1 - 1];
+            if (col1 - 1 >= 0)
+                topBottomSquare = board[row2, col1 - 1];
 
             int duplicateSquare = 0;
             if (col1 - 1 >= 0 && row1 - 1 >= 0)
-                duplicateSquare = board[col1 - 1, row1 - 1];
+                duplicateSquare = board[row1 - 1, col1 - 1];
 
-            int result = board[col2, row2] - leftRightSquare - topBottomSquare + duplicateSquare;
+            int result = board[row2,col2] - leftRightSquare - topBottomSquare + duplicateSquare;
 
             sb.AppendLine(result.ToString());
         }
