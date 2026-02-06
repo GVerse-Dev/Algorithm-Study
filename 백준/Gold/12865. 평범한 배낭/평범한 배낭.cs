d36d8d21f3099f200769a8e3227c4e4276c82c51
GVerse-Dev <44476140+GVerse-Dev@users.noreply.github.com>
2026-02-07 01:37:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 72 ms, Memory: 6648 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cs
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cs
@@ -8,65 +8,44 @@ class BOJ
     static void Main()
     {
         string[] inputNK = Console.ReadLine().Split(" ");
-        ulong n = ulong.Parse(inputNK[0]);
-        ulong k = ulong.Parse(inputNK[1]);
+        int n = int.Parse(inputNK[0]);
+        int k = int.Parse(inputNK[1]);
 
-        Dictionary<ulong, ulong> dic = new Dictionary<ulong,ulong>();
+        int[] dp = new int[k + 1];
 
-        for (ulong i = 0; i < n; i++)
+        for (int i = 1; i < n + 1; i++)
         {
             string[] input = Console.ReadLine().Split(" ");
 
-            ulong weight = ulong.Parse(input[0]);
-            ulong value = ulong.Parse(input[1]);
+            int weight = int.Parse(input[0]);
+            int value = int.Parse(input[1]);
 
-            if (weight > k)
-                continue;
+            //내 배낭의 최대 허용 무게 k가 입력으로 들어온 물건의 무게 weight 보다 커야지만 의미가있으니 
+            //for문은 k 가 weight 보다 크거나 같아야한다.
 
-            var list = dic.ToList();
+            //기존의 방식대로 입력이 들어올 때마다 가능한 모든 경우의 무게들과 계산하면 불필요한 메모리와 연산이 추가되기 때문에
+            //아래와 같은 계산을 적용하면 효율적으로 가능하다.
+            //지금 물건의 무게를 제외한 무게의 가치 + 지금 물건의 무게의 가치 = > 지금 물건을 넣었을 때의 가치
 
-            foreach (var item in list)
+            //역순으로 접근해야 앞에 있는 무게들의 가치를 중복으로 더하지 않는다
+            //배낭의 무게는 7이고, 물건은 무게가 3인 하나만 있다고 가정해보자.
+            //역방향으로 하지 않고 정방향으로 한다면, 무게가 6일 때를 본다면 dp[3] + dp[3] 이 되기 때문에 같은 물건이 2번 들어가게 된다.
+            for (int j = k; j >= weight; j--)
             {
-                ulong newWeight = item.Key + weight;
-                ulong newValue = item.Value + value;
-
-                if (newWeight > k)
-                    continue;
-
-                if (dic.ContainsKey(newWeight) == false)
+                if (dp[j - weight] + value > dp[j])
                 {
-                    dic[newWeight] = newValue;
-                }
-                else
-                {
-                    if (dic[newWeight] < newValue)
-                    {
-                        dic[newWeight] = newValue;
-                    }
+                    dp[j] = dp[j - weight] + value;
                 }
             }
 
-            if (dic.ContainsKey(weight) == false)
-            {
-                dic[weight] = value;
-            }
-            else
-            {
-                if (dic[weight] < value)
-                {
-                    dic[weight] = value;
-                }
-            }
+           
         }
 
-        ulong result = 0;
-        foreach (var item in dic)
+        int result = 0;
+        foreach (var item in dp)
         {
-            if (item.Key <= k)
-            {
-                if (result < item.Value)
-                    result = item.Value;
-            }
+            if (result < item)
+                result = item;
         }
 
         Console.WriteLine(result.ToString());
