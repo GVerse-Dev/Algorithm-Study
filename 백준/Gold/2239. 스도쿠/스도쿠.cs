fe2b0275ef02834bc5430e7139e12dc47387207f
GVerse-Dev <44476140+GVerse-Dev@users.noreply.github.com>
2026-02-28 01:10:47 +0900

    [Gold IV] Title: 스도쿠, Time: 432 ms, Memory: 5424 KB -BaekjoonHub

---
+++ 백준/Gold/2239. 스도쿠/스도쿠.cs
@@ -0,0 +1,130 @@
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+class BOJ
+{
+    static StringBuilder sb = new StringBuilder();
+
+    //행
+    static bool[,] c1 = new bool[9, 10];
+
+    //열
+    static bool[,] c2 = new bool[9, 10];
+
+    //스퀘어
+    static bool[,] c3 = new bool[9, 10];
+
+    static int[,] sudoku = new int[9, 9];
+
+    static int square(int x, int y)
+    {
+        //x
+        //0,1,2 = 0 //3,4,5 = 1  //6,7,8 = 2
+
+        //y
+        //0,1,2 = 0 //3,4,5 = 1  //6,7,8 = 2
+
+        //square
+        // 0(0,0) 1(1,0) 2(2,0)
+        // 3(0,1) 4(1,1) 5(2,1)
+        // 6(0,2) 7(1,2) 8(2,2)
+
+        return (x / 3) + ((y / 3) * 3);
+    }
+
+    static bool DFS(int x, int y)
+    {
+        for (int num = 1; num <= 9; num++)
+        {
+            int nextX = (x + 1) % 9;
+            int nextY = (nextX == 0) ? y + 1 : y;
+
+            //이미 값이 들어가있다면 패스
+            if (sudoku[y,x] != 0)
+            {
+                //마지막이라면
+                if (x == 8 && y == 8)
+                    return true;
+
+                return DFS(nextX, nextY);
+            }
+
+            //행, 열, 스퀘어 모두 num 이 없다면
+            if (!c1[y, num] && !c2[x, num] && !c3[square(x, y), num])
+            {
+                //마지막칸에 빈 숫자라면 확정된거니 넣고 반환
+                if (x == 8 && y == 8)
+                {
+                    c1[y, num] = true;
+                    c2[x, num] = true;
+                    c3[square(x, y), num] = true;
+                    sudoku[y, x] = num;
+                    return true;
+                }
+
+                //문제가 없어보인다면 값을 넣고 다음칸
+                c1[y, num] = true;
+                c2[x, num] = true;
+                c3[square(x, y), num] = true;
+                sudoku[y, x] = num;
+
+                //다음칸에도 문제가 없었다면
+                if (DFS(nextX, nextY))
+                {
+                    return true;
+                }
+                //문제가 있었다면 초기화
+                else
+                {
+                    c1[y, num] = false;
+                    c2[x, num] = false;
+                    c3[square(x, y), num] = false;
+                    sudoku[y, x] = 0;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    static void Main()
+    {
+        for (int y = 0; y < 9; y++)
+        {
+            string s = Console.ReadLine();
+
+            for (int x = 0; x < 9; x++)
+            {
+                int num = s[x] - '0';
+                
+                sudoku[y, x] = num;
+
+                if (num > 0)
+                {
+                    c1[y, num] = true;
+                    c2[x, num] = true;
+                    c3[square(x, y), num] = true;
+                }
+            }
+        }
+
+        DFS(0, 0);
+
+        for (int y = 0; y < 9; y++)
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                sb.Append(sudoku[y, x]);
+            }
+            sb.AppendLine();
+        }
+
+
+        Console.WriteLine(sb.ToString());
+    }
+
+ 
+}
