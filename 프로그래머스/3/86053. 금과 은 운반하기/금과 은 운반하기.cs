4b8d1296efce5ab3b1bd285693a28f4d73eb2378
GVerse-Dev <44476140+GVerse-Dev@users.noreply.github.com>
2026-04-27 22:57:29 +0900

    [level 3] Title: 금과 은 운반하기, Time: 91.67 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.cs
@@ -0,0 +1,55 @@
+using System;
+
+public class Solution {
+    public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t)
+ {
+     long answer = -1;
+
+     long timeMax = (long)4e14;
+     long timeMin = 0;
+
+     while (timeMin < timeMax) 
+     {
+         long timeMid = (timeMax + timeMin) / 2;
+
+         long totalLoad = 0;
+         long totalGoldLoad = 0;
+         long totalSilverLoad = 0;
+
+         for (int i = 0; i < g.Length; i++)
+         {
+             //운반 횟수
+             long carryCount = (timeMid + t[i]) / (2 * t[i]);
+
+             //운반가능한 최대 무게
+             long maxLoad = Math.Min(g[i] + s[i], carryCount * w[i]);
+             long goldLoad = Math.Min(g[i], carryCount * w[i]);
+             long silverLoad = Math.Min(s[i], carryCount * w[i]);
+
+             totalLoad += maxLoad;
+             totalGoldLoad += goldLoad;
+             totalSilverLoad += silverLoad;
+         }
+
+         bool condition1 = (a + b) <= totalLoad;
+         bool condition2 = a <= totalGoldLoad;
+         bool condition3 = b <= totalSilverLoad;
+
+         if (condition1 && condition2 && condition3)
+         {
+             timeMax = timeMid;
+         }
+         else
+         {
+             timeMin = timeMid + 1;
+         }
+
+     }
+
+     answer = timeMin;
+
+     Console.WriteLine(answer.ToString());
+
+     return answer;
+ }
+}
